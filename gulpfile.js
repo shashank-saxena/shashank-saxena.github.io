fd9b196cc53062706fdfd5bf83570713c7bb1ff6
Shashank Saxena <shashank@bellurbis.com>
2020-11-12 21:41:58 +0530

    new build

--- gulpfile.js
+++ gulpfile.js
@@ -63,7 +63,7 @@ gulp.task('minify-js', function() {
 // Copy vendor files from /node_modules into /vendor
 // NOTE: requires `npm install` before running!
 gulp.task('copy', function() {
-  gulp.src([
+  return gulp.src([
       'node_modules/bootstrap/dist/**/*',
       '!**/npm.js',
       '!**/bootstrap-theme.*',
@@ -71,13 +71,13 @@ gulp.task('copy', function() {
     ])
     .pipe(gulp.dest('vendor/bootstrap'))
 
-  gulp.src(['node_modules/jquery/dist/jquery.js', 'node_modules/jquery/dist/jquery.min.js'])
+  return gulp.src(['node_modules/jquery/dist/jquery.js', 'node_modules/jquery/dist/jquery.min.js'])
     .pipe(gulp.dest('vendor/jquery'))
 
-  gulp.src(['node_modules/jquery.easing/*.js'])
+  return gulp.src(['node_modules/jquery.easing/*.js'])
     .pipe(gulp.dest('vendor/jquery-easing'))
 
-  gulp.src([
+  return gulp.src([
       'node_modules/font-awesome/**',
       '!node_modules/font-awesome/**/*.map',
       '!node_modules/font-awesome/.npmignore',
@@ -87,7 +87,7 @@ gulp.task('copy', function() {
     ])
     .pipe(gulp.dest('vendor/font-awesome'))
 
-  gulp.src([
+  return gulp.src([
       'node_modules/devicons/**/*',
       '!node_modules/devicons/*.json',
       '!node_modules/devicons/*.md',
@@ -98,7 +98,7 @@ gulp.task('copy', function() {
     ])
     .pipe(gulp.dest('vendor/devicons'))
 
-  gulp.src(['node_modules/simple-line-icons/**/*', '!node_modules/simple-line-icons/*.json', '!node_modules/simple-line-icons/*.md'])
+  return gulp.src(['node_modules/simple-line-icons/**/*', '!node_modules/simple-line-icons/*.json', '!node_modules/simple-line-icons/*.md'])
     .pipe(gulp.dest('vendor/simple-line-icons'))
 })
 
